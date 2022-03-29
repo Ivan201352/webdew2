36fdc5ac478418348a315b841a38f2cb71437029
Ivan201352 <72215396+Ivan201352@users.noreply.github.com>
2022-03-29 23:08:26 +0300

    Add files via upload

---
+++ Lab1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+application = app
+
+@app.route('/')
+def index():
+    msg = 'Hello'
+    return render_template('index.html', message=msg)
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
